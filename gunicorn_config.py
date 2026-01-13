53495e0d4076865526aa943cdcaf374ed40fe077
99rahul8605-ops <99rahul8605@gmail.com>
2026-01-14 00:38:26 +0530

    Create gunicorn_config.py

---
+++ gunicorn_config.py
@@ -0,0 +1,51 @@
+import multiprocessing
+
+# Server socket
+bind = "0.0.0.0:5000"
+backlog = 2048
+
+# Worker processes
+workers = 1
+worker_class = "sync"
+worker_connections = 1000
+timeout = 120
+keepalive = 2
+
+# Logging
+accesslog = "-"
+errorlog = "-"
+loglevel = "info"
+
+# Process naming
+proc_name = "telegram_video_bot"
+
+# Server mechanics
+daemon = False
+pidfile = None
+umask = 0
+user = None
+group = None
+tmp_upload_dir = None
+
+# SSL
+# keyfile = None
+# certfile = None
+
+# Server hooks
+def post_fork(server, worker):
+    server.log.info("Worker spawned (pid: %s)", worker.pid)
+
+def pre_fork(server, worker):
+    pass
+
+def pre_exec(server):
+    server.log.info("Forked child, re-executing.")
+
+def when_ready(server):
+    server.log.info("Server is ready. Spawning workers")
+
+def worker_int(worker):
+    worker.log.info("Worker received INT or QUIT signal")
+
+def worker_abort(worker):
+    worker.log.info("Worker received SIGABRT signal")
